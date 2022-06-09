201fa69010f55c97eaa9c2146a6b5660f3c85661
Felix Gnass <fgnass@gmail.com>
2022-06-10 00:11:35 +0200

    ci: use node 16.13

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,10 @@ import capri from "@capri-js/preact";
 import preact from "@preact/preset-vite";
 
 export default {
-  build: {
-    dynamicImportVarsOptions: {
-      exclude: /\.mjs$/,
-    },
-  },
   plugins: [
     preact(),
     capri({
       spa: "/preview",
     }),
-    {
-      name: "print-options",
-      options(options) {
-        console.log(
-          "ENV -----------------------------------------------------"
-        );
-        console.log(process.env);
-        console.log(
-          "OPTIONS -------------------------------------------------"
-        );
-        console.log(options);
-      },
-    },
   ],
 };
